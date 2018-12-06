faa790741fe09cd2f9abb087ce6078e48ccbf95c
Manh Tan Nguyen <manh_tan_nguyen@epam.com>
2018-12-06 11:01:56 -0500

    Remove a bunch of features for safety and try to speed up day5, not that much though

--- 2018/Day05/main.c
+++ 2018/Day05/main.c
@@ -5,12 +5,13 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Change this to input.h for the real input data or sample.h to use the sample
 // data that AoC provided as coding example
 #include "input.h"
 
-const int MAX_STRING_LEN = 60000;
+const int MAX_STRING_LEN = 9240;
 
 // Change the following 2 values to 0 (the number, not the char)
 // respectively to use the program for part 1 of our problem
@@ -25,49 +26,33 @@ const int TARGET_CHAR_END = 0;
  *   if can eliminate, return 1
  */
 int collapsible(char a, char b){
-    if (a + 32 == b || b + 32 == a){
-        return 1;
-    }
-    return 0;
+    return abs(a - b) == 32;
 }
 
 int main(){
     char cur_char;
-    char *stack = malloc(MAX_STRING_LEN * sizeof(char));
+    char stack[MAX_STRING_LEN];
+    stack[0] = ' ';
     int min_len = INPUT_STRING_LEN;
-
-    for (char target_char = TARGET_CHAR_BEGIN; target_char < TARGET_CHAR_END + 1; target_char++) {
-        // memset(stack, 0, MAX_STRING_LEN);
-        int stack_current_size = 0;
-        for(int i = 0; i < INPUT_STRING_LEN; i++){
-            cur_char = INPUT_STRING[i];
-            if (cur_char == target_char || cur_char == target_char + 32){
-                // This is a character that we want to eliminate from the very
-                // beginning - part 2 of the challen
-                continue;
-            }
-            if (0 == stack_current_size){
-                // Nothing in the stack - only push
-                stack[stack_current_size++] = cur_char;
-            } else {
-                // Something is already in the stack
-                if (1 == collapsible(stack[stack_current_size - 1], cur_char)){
-                    // This can be eliminated
-                    stack_current_size--;
-                } else {
-                    // Cannot eliminate - just push
-                    stack[stack_current_size++] = cur_char;
-                }
-            }
-            // printf("%s\n", stack);
+    clock_t tv1, tv2;
+    tv1=clock();
+    int stack_current_size = 1;
+    for(int i = 0; i < INPUT_STRING_LEN; i++){
+        cur_char = INPUT_STRING[i];
+        // Something is already in the stack
+        if (1 == collapsible(stack[stack_current_size - 1], cur_char)){
+            // This can be eliminated
+            stack_current_size--;
+        } else {
+            // Cannot eliminate - just push
+            stack[stack_current_size++] = cur_char;
         }
-        // printf("Char %c, remaining len: %d\n", target_char, stack_current_size);
-        // Make sure min_len is the answer value
-        min_len = min_len > stack_current_size? stack_current_size : min_len;
+        // printf("%s\n", stack);
     }
 
-    printf("Answer: %d\n", min_len);
+    printf("Answer: %d\n", stack_current_size - 1);
+    tv2=clock();
+    printf("Execution time: %f seconds\n", (double) (tv2 - tv1) / CLOCKS_PER_SEC);
 
-    free(stack);
     return 0;
 }
